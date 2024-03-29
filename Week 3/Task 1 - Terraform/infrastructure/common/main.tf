2f5a4651ea771f040d09848470e73f07cfbee52f
KinKinoV <mr.kolbasa.ask@gmail.com>
2024-03-29 17:48:27 +0200

    Configured providers, remote backend (S3) and amazon linux 2 ami data

--- Week 3/Task 1 - Terraform/infrastructure/common/main.tf
+++ Week 3/Task 1 - Terraform/infrastructure/common/main.tf
@@ -1,15 +1,51 @@
 terraform {
+  required_version = ">= 1.7.5"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.42"
+        source = "hashicorp/aws"
+        version = ">= 5.32"
     }
   }
 
-  required_version = "~> 1.7.5"
+  backend "s3" {
+    bucket = "my-all-terraform-states"
+    key = "/<environment_name>/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt = true
+  }
 }
 
 provider "aws" {
   profile = "default"
-  region = "us-east-1"
+  region  = "us-east-1"
+}
+
+locals {
+  tags = {
+    Terraform = "true"
+    Environment = "${var.env}"
+  }
+}
+
+data "aws_ami" "amzlinux" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
+  filter {
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
 }
